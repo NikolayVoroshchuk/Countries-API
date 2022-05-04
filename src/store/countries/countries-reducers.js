5f28443372d589e9846344cef9b509942242c0f7
Николай Ворощук <nikolay.voroshchuk98@gmail.com>
2022-05-04 11:33:41 +0300

    upd

---
+++ src/store/countries/countries-reducers.js
@@ -0,0 +1,31 @@
+import {SET_LOADING, SET_ERROR, SET_COUNTRIES} from './countries-actions';
+
+const initialState = {
+    status: 'idle',
+    error: null,
+    list: [],
+}
+
+export const countriesReducer = (state = initialState, {type, payload}) => {
+    switch (type) {
+        case SET_LOADING:
+            return {
+                ...state,
+                status: 'loading',
+                error: null,
+            }
+        case SET_ERROR:
+            return {
+                ...state,
+                status: 'ejected',
+                error: payload,
+            }
+        case SET_COUNTRIES:
+            return {
+                ...state,
+                status: 'received',
+                list: payload,
+            }
+        default: return state
+    }
+}
